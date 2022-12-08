740c1db4e3dc7f8bf57e4272381823aa6d28844b
Daniel <daniel@djt.id.au>
2022-12-09 08:10:09 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,48 +1,37 @@
 import torch
 from torch import autocast
-from diffusers import StableDiffusionPipeline, LMSDiscreteScheduler
+from diffusers import StableDiffusionPipeline
 import base64
 from io import BytesIO
 import os
 
-# Init is ran on server startup
-# Load your model to GPU as a global variable here using the variable name "model"
 def init():
     global model
     HF_AUTH_TOKEN = os.getenv("HF_AUTH_TOKEN")
-    
-    # this will substitute the default PNDM scheduler for K-LMS  
-    lms = LMSDiscreteScheduler(beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear")
+    model = StableDiffusionPipeline.from_pretrained('stabilityai/stable-diffusion-2-1-base', 
+                                              torch_dtype=torch.float16, 
+                                              revision="fp16",
+                                              use_auth_token=HF_AUTH_TOKEN).to("cuda")    
 
-    model = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", scheduler=lms, use_auth_token=HF_AUTH_TOKEN).to("cuda")
-
-# Inference is ran for every server call
-# Reference your preloaded global model variable here.
 def inference(model_inputs:dict) -> dict:
     global model
 
-    # Parse out your arguments
     prompt = model_inputs.get('prompt', None)
-    height = model_inputs.get('height', 512)
-    width = model_inputs.get('width', 512)
-    num_inference_steps = model_inputs.get('num_inference_steps', 50)
-    guidance_scale = model_inputs.get('guidance_scale', 7.5)
-    input_seed = model_inputs.get("seed",None)
+    height = model_inputs.get('height', 768)
+    width = model_inputs.get('width', 768)
+    steps = model_inputs.get('steos', 5)
+    guidance_scale = model_inputs.get('guidance_scale', 9)
+    seed = model_inputs.get("seed", None)
+
+    if not prompt: return {'message': 'No prompt was provided'}
     
-    #If "seed" is not sent, we won't specify a seed in the call
     generator = None
-    if input_seed != None:
-        generator = torch.Generator("cuda").manual_seed(input_seed)
+    if seed: generator = torch.Generator("cuda").manual_seed(seed)
     
-    if prompt == None:
-        return {'message': "No prompt provided"}
-    
-    # Run the model
-    with autocast("cuda"):
-        image = model(prompt,height=height,width=width,num_inference_steps=num_inference_steps,guidance_scale=guidance_scale,generator=generator)["sample"][0]
+    image = pipe(prompt, guidance_scale=guidance_scale, height=height, width=width, num_inference_steps=steps, generator=generator).images[0]
     
     buffered = BytesIO()
-    image.save(buffered,format="JPEG")
+    image.save(buffered, format="JPEG")
     image_base64 = base64.b64encode(buffered.getvalue()).decode('utf-8')
 
     # Return the results as a dictionary
