64b146470d40715edb5e0b59b2bd50736a4c8ea8
Daniel <daniel@djt.id.au>
2023-01-30 23:10:03 +1000

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,1 +1,19 @@
 
+import banana_dev as banana
+
+model_inputs = {
+  "prompt": "A superhero with the body of a banana",
+  "height": 768,
+  "width": 768,
+  "steps": 20,
+  "guidance_scale": 9,
+  "seed": None
+}
+
+api_key = "YOUR_API_KEY"
+model_key = "YOUR_MODEL_KEY"
+
+# Run the model
+out = banana.run(api_key, model_key, model_inputs)
+
+
